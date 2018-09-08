e10bc45b10a55a24c64f04a0287a73cd782713e4
Andrii Dziuba <Andrii.Dziuba@lebara.com>
2018-09-08 17:58:30 +0300

    Test repo

---
+++ js/slider.js
@@ -0,0 +1,11 @@
+$('.js-slider').slick({
+	infinite: true,
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	speed: 500,
+	autoplay: true,
+	arrows: false,
+	swipe: true,
+	fade: true,
+	autoplaySpeed: 3000
+})
